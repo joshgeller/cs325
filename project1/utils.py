e924d1fe401d79e7a226a3cf459f11de63b914fa
Josh Geller <joshualgeller@gmail.com>
2016-01-19 07:53:30 -0500

    Update utils

--- project1/utils.py
+++ project1/utils.py
@@ -7,16 +7,14 @@ def load_problems():
     :return: array of arrays containing integers, e.g.
 
     [[1,2,3],[4,5,6],[7,8,9]]
-
     """
     problems = []
     with open('tests/MSS_Problems.txt') as f:
         for line in f:
-            line = line.replace('[', '').replace(']', '').replace(',', '')
+            line = line.replace('[', '').replace(']', '').replace(' ', '')
             problems.append(
-                [int(num) for num in line.split(' ') if num not in ' \n'])
+                [int(num) for num in line.split(',') if num not in '\n'])
     return problems
-
 
 def write_results(filename, original_array, max_subarray, max_sum):
     """
