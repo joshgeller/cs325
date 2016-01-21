bbdc2540b61115524b27c49b56353e10fc8452da
CaptainKimothy <mcleodki@onid.oregonstate.edu>
2016-01-20 23:32:09 -0600

    Added better_enum_max_subarray

---
+++ project1/better_enum_max_subarray.py
@@ -0,0 +1,35 @@
+from utils import load_problems, write_results
+
+
+"""
+Based on pseudocode from Glencora Borradaile's
+"Designing Poly-Time Algorithms" lecture
+"""
+
+
+def better_enum_max_subarray(ls):
+	maxSum = newSum = 0
+	low = high = 0
+	i = 0
+	for i in range len(ls):
+		newSum = 0
+		for j in range(i, len(ls)):
+			newSum = newSum + ls[j]
+			if newSum > maxSum:
+				maxSum = newSum
+				low = i
+				high = j
+	return maxSum, low, high
+
+
+if __name__ == '__main__':
+	problems = load_problems()
+    	for problem in problems
+		results = better_enum_max_subarray(ls = problem)
+        	write_results(
+            		filename='MSS_Results.txt',
+            		original_array=problem,
+           	  	max_subarray=problem[results[1]:results[2] + 1],
+            		max_sum=results1[0]
+
+        )
