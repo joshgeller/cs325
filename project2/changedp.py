91e68b8d536d105c1a3678b9ac4c89de5d888768
Josh Geller <joshualgeller@gmail.com>
2016-01-27 19:21:29 -0500

    Cleanup

--- project2/changedp.py
+++ project2/changedp.py
@@ -4,26 +4,31 @@ def changedp(coins, change):
     # e.g. let change=5, min_coins = [0, 0, 0, 0, 0]
     min_coins = [0] * (change + 1)
 
+    coins_used = [0] * (change + 1)
+
     # bottom-up approach - solve small sub-problems first, from [0..change]
     for change_subproblem in range(change + 1):
 
-        # worst-case scenario: we need one coin for every cent in the sub-problem
+        # base case: one cent coin needed for every cent in the sub-problem
         coins_needed = change_subproblem
+        last_coin_used = 1
 
         # use optimal sub-structure of sub-problems to determine the
         # minimum number of coins required for the current sub-problem
-        for coin_value in coins:
-            if coin_value <= change_subproblem:
-                coins_needed = min(
-                    coins_needed,
-                    1 + min_coins[change_subproblem - coin_value]
-                )
+        for coin in coins:
+            if coin <= change_subproblem:
+                if 1 + min_coins[change_subproblem - coin] < coins_needed:
+                    coins_needed = 1 + min_coins[change_subproblem - coin]
+                    last_coin_used = coin
 
         # store the sub-problem solution in the table
         min_coins[change_subproblem] = coins_needed
 
-    return min_coins[change]
+        # store the last coin used
+        coins_used[change_subproblem] = last_coin_used
+
+    return coins_used, min_coins[change]
 
 
 if __name__ == '__main__':
-    print(changedp([1, 3, 4], 11))
+    print(changedp([1, 5, 10, 21, 25], 63))
