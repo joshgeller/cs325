927f2c994d47dbc5c4e585c719898a265f610b1e
Josh Geller <joshualgeller@gmail.com>
2016-01-28 07:51:46 -0500

    Remove trailing whitespace, update utils

--- project2/utils.py
+++ project2/utils.py
@@ -3,17 +3,46 @@
 def load_problems():
     """
     Loads problem data from Coin1.txt.
-    Converts each line to an array containing integers.
-    All arrays are then appended to a single containing array.
+    Reads two lines at a time to extract full problem data.
 
-    :return: array of arrays containing integers, e.g.
+    :return: array of tuples, each tuple represents a coin change problem
 
-    [[1,2,3],[4,5,6],[7,8,9]]
+    [([1, 5, 10, 25, 50], 75), ([1, 10, 21, 50], 63), ([1, 2, 4, 8, 16], 120)]
+
     """
     problems = []
-    with open('tests/Coin1.txt') as f:
-        for line in f:
-            line = line.replace('[', '').replace(']', '').replace(' ', '')
-            problems.append(
-                [int(num) for num in line.split(',') if num not in '\n'])
+    f = open('tests/Coin1.txt', 'r')
+    while 1:
+        try:
+            coins = f.readline()
+            if coins:
+                coins = [int(num) for num in coins.replace('[', '').replace(']', '').replace(' ', '').split(',') if num not in '\n']
+            else:
+                break
+            change = f.readline()
+            if change:
+                change = change.replace('\n', '')
+                change = int(change)
+            else:
+                break
+        except Exception:
+            break
+
+        problems.append((coins, change))
+
     return problems
+
+def write_results(filename, algorithm_name, qty_array, min_coins):
+    """
+    Writes results to file in proper format.
+
+    :param filename: output filename
+    :param algorithm_name: name of algorithm to categorize results file
+    :param qty_array: results array representing qty of each coin used
+    :param min_coins: integer representing min # of coins used for problem
+    :return: outputs file to disk
+    """
+    with open(filename, 'a') as f:
+        f.write('{0}\n'.format(algorithm_name))
+        f.write('{0}\n'.format(qty_array))
+        f.write('{0}\n\n'.format(min_coins))
