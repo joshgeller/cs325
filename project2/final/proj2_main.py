961dec0be318da7bfb7dc2406c4c36078aa54209
Tim Thomas <tim.thomas@areva.com>
2016-02-02 11:05:17 -0500

    Added updated proj2_main.py to final files folder.

--- project2/final/proj2_main.py
+++ project2/final/proj2_main.py
@@ -130,17 +130,15 @@ def changeslow(V, K, total):
 
     if K in V:
         min_coins[V.index(K)] += 1
-        return min_coins, total
+        return min_coins, sum(min_coins)
 
     min_coins[0] = K
-
     for i in [coin for coin in V if coin <= K]:
         subproblem_qty, dummy = changeslow(V, K - i, total)
         subproblem_qty[V.index(i)] += 1
         if sum(min_coins) > sum(subproblem_qty):
             min_coins = subproblem_qty
-            total = sum(min_coins)
-    return min_coins, total
+    return min_coins, sum(min_coins) 
 
 
 def main(filename):
