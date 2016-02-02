55560946b6d6a05ad072c360c8ff946972e9daae
Tim Thomas <tim.thomas@areva.com>
2016-02-02 17:54:27 -0500

    Final version used to create plots in report.

--- project2/proj2_analysis.py
+++ project2/proj2_analysis.py
@@ -29,7 +29,7 @@ def plot_coins(data, part_num):
 
     marks = list('o*^')
     for algo in algos: 
-        x = [i for i in data['A_list']]
+        x = data['A_list']
         y = data[algo]['num']
         plt.plot(x, y, linestyle='None', marker=marks.pop(), label=algo)
 
@@ -50,40 +50,44 @@ def plot_times(data, part_num):
 
     if min(data['A_list']) > CHANGESLOW_MAX_A: 
         algos = ['changegreedy', 'changedp']
+        f, axes = plt.subplots(2, 1)
     else:
         algos = ['changegreedy', 'changedp', 'changeslow']
+        f, axes = plt.subplots(3, 1)
 
     marks = list('o*^')
-    min_y, max_y = 1.0, 0.0  # used to set plot limits 
-    for algo in algos: 
+
+    for algo,ax in zip(algos,axes): 
+        min_y, max_y = 1.0, 0.0  # used to set plot limits 
         x = [i for i in data['A_list']]
         y = data[algo]['times']
         if min(y) < min_y:
             min_y = min(y)
         if max(y) > max_y:
             max_y = max(y)
-        plt.plot(x, y, linestyle='None', marker=marks.pop(), label=algo)
+        ax.plot(x, y, linestyle='None', marker=marks.pop(), label=algo)
+        ax.set_xlim(min(x)-1, max(x)+10)
+        ax.set_ylim(min_y*.70, max_y*1.20)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.tick_params(axis='both', which='both')
+        ax.xaxis.set_minor_formatter(FormatStrFormatter('%.2e'))
+        #ax.xaxis.set_major_locator(plt.NullLocator())
+        if min(data['A_list']) > CHANGESLOW_MAX_A: 
+            ax.xaxis.set_minor_locator(plt.FixedLocator(
+                                    range(min(x), max(x)+200, 1000)))
+        else:
+            ax.xaxis.set_minor_locator(plt.FixedLocator(
+                                    range(min(x), max(x)+5, 10)))
+        if algo != 'changeslow':
+            ax.yaxis.set_minor_formatter(FormatStrFormatter('%.1e'))
+        ax.grid(True, which='both')
+        ax.set_ylabel('Time (sec)')
+        ax.legend(loc=2)
 
-    plt.xlabel('Amount of Change to Make (A) - Log Scale')
-    plt.ylabel('Run Time (sec) - Log Scale')
-    plt.xlim(min(x)*.98, max(x)*1.02)
-    plt.ylim(min_y*.90, max_y*1.10)
-    plt.xscale('log')
-    plt.yscale('log')
-    ax = plt.gca()
-    plt.tick_params(axis='x', which='both')
-    ax.xaxis.set_minor_formatter(FormatStrFormatter('%d'))
-    ax.xaxis.set_major_locator(plt.NullLocator())
-    if min(data['A_list']) > CHANGESLOW_MAX_A: 
-        ax.xaxis.set_minor_locator(plt.FixedLocator(
-                                   range(min(x), max(x)+100, 100)))
-    else:
-        ax.xaxis.set_minor_locator(plt.FixedLocator(
-                                   range(min(x), max(x)+5, 5)))
-    plt.title('{}'.format(part_num))
-    plt.legend(loc='best')
-    plt.grid(True, which="both")
-    plt.savefig('Times_{}.png'.format(part_num))
+    axes[0].set_title('{}'.format(part_num))
+    axes[-1].set_xlabel('Amount of Change to Make (A) - Log Scale')
+    f.savefig('Times_{}.png'.format(part_num))
 
     return
 
@@ -98,26 +102,24 @@ def plot_time_vs_num_denom(data):
             if algo == 'changeslow' and min(data[part]['A_list']) > CHANGESLOW_MAX_A:
                 continue
             else:
-                x = len(data[part]['V'])  # number of denominations
-                y = np.mean(data[part][algo]['times'])
-                if y < min_y:
-                    min_y = y 
-                if y > max_y:
-                    max_y = y 
-                plt.plot(x, y, 'bo')
+                y = data[part][algo]['times']
+                x = [len(data[part]['V'])] * len(y)  # number of denominations
+                if min(y) < min_y:
+                    min_y = min(y)
+                if max(y) > max_y:
+                    max_y = max(y)
+                plt.plot(x, y, 'b.')
 
         plt.xlabel('Number of Available Denominations')
         plt.ylabel('Run Time (sec)')
         plt.xlim(4, 17)
-        #plt.ylim(min_y*.95, max_y*1.05)
-        #plt.ylim(0.0, max_y*1.05)
         plt.yscale('log')
         plt.title('Part 8: {}'.format(algo))
         plt.grid()
         plt.savefig('8_{}.png'.format(algo))
 
     return
-   
+
 
 def main():
 
@@ -125,21 +127,21 @@ def main():
              'changeslow' : proj2_main.changeslow,
              'changedp' : proj2_main.changedp}
 
-    experiments = {'Part 4 -- A = 2010, 2015, 2020,...,2200' : 
+    experiments = {'Part 4 -- A = 2010, 2015, 2020,...,5000' : 
                           {'V' : [1, 5, 10, 25, 50], 
-                           'A_list' : range(2010, 2205, 5),
+                           'A_list' : range(2010, 5005, 5),
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
-                    'Part 4 -- A = 5, 10, 15,...,50' :
+                    'Part 4 -- A = 5, 6, 7,...40' :
                           {'V' : [1, 5, 10, 25, 50], 
-                           'A_list' : range(5, 55, 5),
+                           'A_list' : range(5, 41, 1),
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
-                    'Part 5 -- with V1 and A = 2000, 2001, 2002,...,2200' :
+                    'Part 5 -- with V1 and A = 2000, 2001, 2002,...,5000' :
                           {'V' : [1, 2, 6, 12, 24, 48, 60], 
-                           'A_list' : range(2000, 2201, 1),
+                           'A_list' : range(2000, 5001, 1),
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
@@ -149,9 +151,9 @@ def main():
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
-                    'Part 5 -- with V2 and A = 2000, 2001, 2002,...,2200' :
+                    'Part 5 -- with V2 and A = 2000, 2001, 2002,...,5000' :
                           {'V' : [1, 6, 13, 37, 150],       
-                           'A_list' : range(2000, 2201, 1),
+                           'A_list' : range(2000, 5001, 1),
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
@@ -161,9 +163,9 @@ def main():
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}}, 
-                    'Part 6 -- A = 2000, 2001, 2002,...,2200' :
+                    'Part 6 -- A = 2000, 2001, 2002,...,5000' :
                           {'V' : [1] + list(range(2, 32, 2)), 
-                           'A_list' : range(2000, 2201, 1),
+                           'A_list' : range(2000, 5001, 1),
                            'changeslow' : {'times' : [], 'num' : []},
                            'changegreedy' : {'times' : [], 'num' : []},
                            'changedp' : {'times' : [], 'num' : []}},
@@ -194,7 +196,6 @@ def main():
         if len(sys.argv) > 1 and sys.argv[1] == '--plot':
             plt.figure()
             plot_coins(experiments[part], part)
-            plt.figure()
             plot_times(experiments[part], part)
 
     plot_time_vs_num_denom(experiments)
