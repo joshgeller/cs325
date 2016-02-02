5c44e76ee20d5a227f0ced66312fd1d12d20aaeb
Josh Geller <joshualgeller@gmail.com>
2016-02-01 19:21:47 -0500

    Tweaked

--- brute_force.py
+++ brute_force.py
@@ -2,12 +2,12 @@
 
 def changeslow(V, K):
 
-    if K in V:
-        qty = [0] * len(V)
-        qty[V.index(K)] += 1
-        return qty
-
     min_coins = [0] * len(V)
+
+    if K in V:
+        min_coins[V.index(K)] += 1
+        return min_coins
+
     min_coins[0] = K
 
     for i in [coin for coin in V if coin <= K]:
@@ -15,8 +15,7 @@ def changeslow(V, K):
         subproblem_qty[V.index(i)] += 1
         if sum(min_coins) > sum(subproblem_qty):
             min_coins = subproblem_qty
-
     return min_coins
 
 if __name__ == '__main__':
-    print(changeslow([1, 5, 10, 25], 13), sum(changeslow([1, 5, 10, 25], 13)))
+    print(changeslow([1, 5, 10, 12], 29), sum(changeslow([1, 5, 10, 12], 29)))
