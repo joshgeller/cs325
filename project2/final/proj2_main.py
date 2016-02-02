3cee87944dfec1a8f5709a71a7b55cde71b95e0c
Josh Geller <joshualgeller@gmail.com>
2016-02-02 08:27:42 -0500

    Add final files

---
+++ project2/final/proj2_main.py
@@ -0,0 +1,173 @@
+#!/usr/local/bin/python3
+
+import sys
+
+
+def load_problems(filename):
+    """
+    Loads problem data from file.
+    Reads two lines at a time to extract full problem data.
+
+    :return: array of tuples, each tuple represents a coin change problem
+
+    [([1, 5, 10, 25, 50], 75), ([1, 10, 21, 50], 63), ([1, 2, 4, 8, 16], 120)]
+
+    """
+    problems = []
+    f = open(filename, 'r')
+    while 1:
+        try:
+            coins = f.readline()
+            if coins:
+                coins = [int(num) for num in coins.replace('[', '')
+                    .replace(']', '')
+                    .replace(' ', '')
+                    .split(',') if num not in '\n']
+            else:
+                break
+            change = f.readline()
+            if change:
+                change = change.replace('\n', '')
+                change = int(change)
+            else:
+                break
+        except Exception:
+            break
+
+        problems.append((coins, change))
+
+    return problems
+
+
+def write_results(filename, algorithm_name, qty_array, min_coins):
+    """
+    Writes results to file in proper format.
+
+    :param filename: output filename
+    :param algorithm_name: name of algorithm to categorize results file
+    :param qty_array: results array representing qty of each coin used
+    :param min_coins: integer representing min # of coins used for problem
+    :return: outputs file to disk
+    """
+    with open(filename, 'a') as f:
+        f.write('{0}\n'.format(algorithm_name))
+        f.write('{0}\n'.format(qty_array))
+        f.write('{0}\n'.format(min_coins))
+
+
+def changegreedy(V, A):
+    """ Naive greedy algorithm."""
+    min = 0
+    qtys = [0] * len(V)
+    change_made = 0
+    i = len(V) - 1
+    while change_made < A:
+        if (V[i] + change_made <= A):
+            change_made += V[i]
+            qtys[i] += 1
+            min += 1
+        else:
+            i -= 1
+
+    return qtys, min
+
+
+def changedp(coins, change):
+    # first, we create an array with n indices, where n = change
+    # min_coins[i] = the minimum number of coins needed to make i change
+    # e.g. let change=5, min_coins = [0, 0, 0, 0, 0]
+    min_coins = [0] * (change + 1)
+
+    # create an array with m indices, where m = change
+    # table used to calculate the quantity of each coin used to make m change
+    # coins_used[i] = the value of the last coin used to make i change
+    # e.g. let change=1, coins_used[i] = 1
+    coins_used = [0] * (change + 1)
+
+    # final results array - quantity used for each coin value
+    # e.g. let coins=[1,5], change=5, qty_used=[0,1]
+    qty_used = [0] * len(coins)
+
+    # bottom-up approach - solve small sub-problems first, from [0..change]
+    for change_subproblem in range(change + 1):
+
+        # base case: zero or one coin needed for every cent in the sub-problem
+        coins_needed = change_subproblem
+        if change_subproblem == 0:
+            last_coin_used = 0
+        else:
+            last_coin_used = 1
+
+        # use optimal sub-structure of sub-problems to determine the
+        # minimum number of coins required for the current sub-problem
+        for coin in coins:
+            if coin <= change_subproblem:
+                if 1 + min_coins[change_subproblem - coin] < coins_needed:
+                    coins_needed = 1 + min_coins[change_subproblem - coin]
+                    last_coin_used = coin
+
+        # store the sub-problem solution in the table
+        min_coins[change_subproblem] = coins_needed
+
+        # store the last coin used
+        coins_used[change_subproblem] = last_coin_used
+
+    # add up how many coins of each value were used
+    coin = change
+    while coin > 0:
+        # get the coin used to make the change at the current position
+        coin_used = coins_used[coin]
+        # increment the quantity used for this denomination
+        qty_used[coins.index(coin_used)] += 1
+        # subtract the value of the coin used to find the next coin
+        coin -= coin_used
+
+    return qty_used, min_coins[change]
+
+
+def changeslow(V, K, total):
+    min_coins = [0] * len(V)
+
+    if K in V:
+        min_coins[V.index(K)] += 1
+        return min_coins, total
+
+    min_coins[0] = K
+
+    for i in [coin for coin in V if coin <= K]:
+        subproblem_qty, dummy = changeslow(V, K - i, total)
+        subproblem_qty[V.index(i)] += 1
+        if sum(min_coins) > sum(subproblem_qty):
+            min_coins = subproblem_qty
+            total = sum(min_coins)
+    return min_coins, total
+
+
+def main(filename):
+    algorithms = {
+        'Greedy': changegreedy,
+        'Brute Force': changeslow,
+        'Dynamic Programming': changedp,
+    }
+
+    problems = load_problems(filename)
+
+    for algorithm_name, algorithm_func in algorithms.items():
+        print('Running {0}...'.format(algorithm_name))
+        for problem in problems:
+            if algorithm_name == 'Brute Force':
+                qty, min = algorithm_func(problem[0], problem[1], 1)
+            else:
+                qty, min = algorithm_func(problem[0], problem[1])
+
+            write_results('{}change.txt'.format(filename.split('.')[0]),
+                          algorithm_name, qty, min)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        main(filename)
+    else:
+        print('Invalid command line arguments')
+        exit(1)
---
+++ project2/final/readme.txt
@@ -0,0 +1,20 @@
+CS352 Group 2 Project 1
+Tim Thomas <thomasti@oregonstate.edu>
+Kimberly McLeod <mcleodki@oregonstate.edu>
+Josh Geller <gellerj@oregonstate.edu>
+
+******************************************
+
+To compile and execute the main program, run the following command:
+
+python3 proj2_main.py [filename]
+
+This will run all 3 algorithms against the problems defined in [filename],
+then output the results to [filename]change.txt.
+
+Note: Problems must be formatted the same way they appear in CoinW16.txt, e.g.
+
+[1, 5, 10, 25, 50]
+75
+
+******************************************
