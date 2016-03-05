eb6d7e36ebe8bef4fb82409395bf32547bb51e05
Tim Thomas <thomasti@oregonstate.edu>
2016-03-05 18:04:34 -0500

    Fixed so it's a true MST implementation

--- project4/2-approx.py
+++ project4/2-approx.py
@@ -21,27 +21,41 @@ def prim(G, r):
     Q = {u : [float('inf'), -1] for u in G_copy.keys()} 
     Q[r] = [0, -1]
     mst = [] 
-    prev = Q[r][1]
     while Q:
         u = min(Q, key=Q.get) 
-        mst.append((prev, u))
-        prev = u
+        mst.append((Q[u][1], u))
         Q.pop(u)
         for v in Q:
             dist_uv = get_distance(G[u][0], G[u][1], G[v][0], G[v][1])
             if dist_uv < Q[v][0]:
                 Q[v][0] = dist_uv
                 Q[v][1] = u 
-    mst.append((prev, r))
     return mst[1:]
+
+def mst_to_tour(mst):
+    """Converts a list of tuples representing edges of an MST to a preorder
+    walk, or tour"""
+    tour = []
+    visited = []
+    for edge in mst:
+        if edge[0] not in visited:
+            tour.append(edge[0])
+            visited.append(edge[0])
+        if edge[1] not in visited:
+            tour.append(edge[1])
+            visited.append(edge[1])
+    tour.append(tour[0])
+    return tour
 
 
 def get_tour_cost(t, cities):
     cost = 0
-    for e in t:
-        x1, y1 = cities[e[0]]
-        x2, y2 = cities[e[1]]
+    prev = t[0]
+    for v in t[1:]:
+        x1, y1 = cities[prev]
+        x2, y2 = cities[v]
         cost += get_distance(x1, y1, x2, y2)
+        prev = v
     return cost 
 
 
@@ -55,8 +69,9 @@ with open(infile, 'r') as f:
 
 #  Try each city as a starting city
 min_cost = float('inf')
-for root in cities.keys():
-    tour = prim(cities, root)
+for root in ['1']cities.keys():
+    mst = prim(cities, root)
+    tour = mst_to_tour(mst)
     cost = get_tour_cost(tour, cities)
     if cost < min_cost:
         min_cost = cost
@@ -67,12 +82,9 @@ print('Best tour cost is {} starting at {}'
       .format(min_cost, cities[best_start]))
 
 x_arr, y_arr = [], []
-for edge in best_tour:
-    x1, y1 = cities[edge[0]]
-    x2, y2 = cities[edge[1]]
-    x_arr.append(x1)
-    x_arr.append(x2)
-    y_arr.append(y1)
-    y_arr.append(y2)
+for city in tour:
+    x, y = cities[city]
+    x_arr.append(x)
+    y_arr.append(y)
 plt.plot(x_arr, y_arr, 'o-')
 plt.show()
