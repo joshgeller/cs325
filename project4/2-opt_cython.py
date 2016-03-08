46ffed67a544e4fdf37569bcbb13998972196647
Tim Thomas <thomasti@oregonstate.edu>
2016-03-08 00:21:02 -0500

    fixed conversion of MST to pre-order traversal

--- project4/2-opt_cython.py
+++ project4/2-opt_cython.py
@@ -41,20 +41,20 @@ def prim(G, r):
     return mst[1:]
 
 
-def mst_to_tour(mst):
-    """Converts a list of tuples representing edges of an MST to a preorder
-    walk, or tour"""
-    tour = []
-    visited = []
-    for edge in mst:
-        if edge[0] not in visited:
-            tour.append(edge[0])
-            visited.append(edge[0])
-        if edge[1] not in visited:
-            tour.append(edge[1])
-            visited.append(edge[1])
-    tour.append(tour[0])
-    return tour
+def dfs(G):
+    for u in G.keys():
+        if u in white:
+            dfs_visit(G, u)
+            
+def dfs_visit(G, u):
+    gray.append(u)
+    white.remove(u)
+    for v in G[u]:
+        if v in white:
+            dfs_visit(G, v)
+    gray.remove(u)
+    T.append(u)
+
 
 infile = sys.argv[1] 
 cities = {}
@@ -66,22 +66,31 @@ with open(infile, 'r') as f:
 # use 2-approx tour as starting point for 2-opt
 root = '1'
 mst = prim(cities, root)
-tour = mst_to_tour(mst)
-best_tour = tour 
-#best_tour = list(cities.keys())
-#best_tour.append(best_tour[-1])
-#two_opt_tour = get_2_opt(best_tour)
-two_opt_tour = best_tour 
+mst_adj = {c:[] for c in cities.keys()}
+for e in mst:
+    mst_adj[e[0]].append(e[1])
+
+# do DFS to get preorder traversal of MST
+white = list(mst_adj.keys())
+gray = []
+T = []
+dfs(mst_adj)
+T.reverse()
+T.append(T[0])
+seed_tour = T
+
+# run 2-opt on seed tour
+two_opt_tour = get_2_opt(seed_tour)
 two_opt_cost = get_tour_cost(two_opt_tour, cities)
-
 print('Best tour cost is {} starting at {}'
-      .format(two_opt_cost, cities[best_tour[0]]))
+      .format(two_opt_cost, cities[two_opt_tour[0]]))
 
+# plot
 x_arr, y_arr = [], []
 for city in two_opt_tour:
     x, y = cities[city]
     x_arr.append(x)
     y_arr.append(y)
-plt.plot(x_arr, y_arr, 'o-')
+plt.plot(x_arr, y_arr, '.-')
 plt.savefig('{}.png'.format(sys.argv[1].split('/')[-1]))
-#plt.show()
+plt.show()
