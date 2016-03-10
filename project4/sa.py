243acd1f61e5b36cb7ee19591e4d338c02a23ff8
Tim Thomas <thomasti@oregonstate.edu>
2016-03-09 21:12:24 -0500

    Added comments, citations, and write output to file.

--- project4/sa.py
+++ project4/sa.py
@@ -1,3 +1,10 @@
+#============================================================================== 
+#    CS 325, Winter 2016.  Project 4.  Traveling Salesman Problem
+#    This program based in large part on methods discussed at:
+#    www.physics.rutgers.edu/~haule/681/MC.pdf
+#    and associated code found at:
+#    www.physics.rutgers.edu/~haule/681/src_MC/python_codes/salesman.py
+#============================================================================== 
 import numpy as np
 import sys
 import math
@@ -9,7 +16,8 @@ def prim(G, r):
     G is a dictionary with city names as keys and x,y coords tuple as values.
     r is the root city name.
 
-    Returns:  list of tuples (u,v) corresponding to edges in the mst from r.
+    Returns:  list of tuples (u,v) corresponding to edges in the MST 
+              using Prim's algorithm starting from r.
     """
     # Q is {label : (key, parent)}
     G_copy = dict(G)
@@ -23,7 +31,8 @@ def prim(G, r):
         mst.append((Q[u][1], u))
         Q.pop(u)
         for v in Q:
-            dist_uv = Distance(np.array((G[u][0], G[u][1])), np.array((G[v][0], G[v][1])))
+            dist_uv = Distance(np.array((G[u][0], G[u][1])), 
+                               np.array((G[v][0], G[v][1])))
             if dist_uv < Q[v][0]:
                 Q[v][0] = dist_uv
                 Q[v][1] = u 
@@ -31,6 +40,14 @@ def prim(G, r):
 
 
 def dfs(G, start):
+    """
+    G is a dictionary with city names as keys and x,y coords tuple as values.
+    start is the root city name.
+
+    Returns:  an ordered list of vertices in G corresponding to the order
+              in which they are processed in the search (i.e. a preorder
+              walk).
+    """
     visited, stack = set(), [start]
     T = []
     while stack:
@@ -42,23 +59,24 @@ def dfs(G, start):
     return T
 
 
-def dfs_visit(G, u, white, gray, T):
-    gray.append(u)
-    white.remove(u)
-    for v in G[u]:
-        if v in white:
-            dfs_visit(G, v, white, gray, T)
-    gray.remove(u)
-    T.append(u)
-    return T, white, gray
+def two_approx(G):
+    """
+    G is a dictionary with city names as keys and x,y coords tuple as values.
 
+    Returns:  an ordered list of vertices in G corresponding to an approximate
+              solution to the minimum cost hamiltonian cycle. 
 
-def two_approx(cities):
-    root = list(cities.keys())[0] 
-    mst = prim(cities, root)
-    #mst_adj = {c:set() for c in cities.keys()}
+    Notes:   First calculates a minimum spanning tree (MST) using Prim's
+             algorithm and then finds the solutions by doing a pre-order 
+             walk (depth first search) of the MST. Guaranteed to give
+             a tour that is at most 2 times the optimal solution.  
+    """
+    root = list(G.keys())[0] 
+    mst = prim(G, root)
     mst_adj = {}
-    for c in cities.keys():
+
+    # convert G to an adjacency list
+    for c in G.keys():
         mst_adj[c] = set()
     for e in mst:
         mst_adj[e[0]].add(e[1])
@@ -68,11 +86,19 @@ def two_approx(cities):
 
 
 def Distance(R1, R2):
+    """ 
+    R1, R2:  numpy arrays containing x,y coorrdinates for a node. 
+
+    Returns:  Distance between R1 and R2 in XY plane.
+    """
     return int(round(math.sqrt(
             math.pow((R1[0] - R2[0]),2) + math.pow((R1[1] - R2[1]),2))))
 
 
 def TotalDistance(tour, R):
+    """
+    Returns the total distance of a tour.
+    """
     dist = 0
     for i in xrange(len(tour) - 1):
         dist += Distance(R[tour[i]], R[tour[i+1]])
@@ -81,10 +107,21 @@ def TotalDistance(tour, R):
 
 
 def two_opt(tour, n):
+    """ 
+    Swaps two edges in a tour.
+    Based on code found at:
+    www.physics.rutgers.edu/~haule/681/src_MC/python_codes/salesman.py
+    """
     tour[n[0]:n[1]+1] = reversed(tour[n[0]:n[1]+1])
     return tour
 
+
 def three_opt(tour, n):
+    """ 
+    Swaps three edges in a tour.
+    Based on code found at:
+    www.physics.rutgers.edu/~haule/681/src_MC/python_codes/salesman.py
+    """
     nct = len(tour)
     newtour=[]
 
@@ -101,15 +138,29 @@ def three_opt(tour, n):
         newtour.append(tour[(j + n[3]) % nct])
     return newtour
 
+
 def anneal(tour, cities):
+    """
+    Performs simulated annealing on a given ordering of vertices (tour)
+    in a graph (cities) with nodes on the XY plane.
+
+    Returns: tour, dist
+        tour = list of cities corresponding to solution
+        dist = total distance of tour
+    Based on code found at:
+    www.physics.rutgers.edu/~haule/681/src_MC/python_codes/salesman.py
+    """
     ncities = len(cities)
     Tstart = 0.3                 # Starting temperature - has to be high enough
     Tend = 0.1                   # Ending temperature - quit when reached
-    fCool = 0.9 + 0.1*ncities/20000.0  # Factor to multiply temperature at each cooling step
+    # fCool is Factor to multiply temperature at each cooling step
+    # It is tuned to provide a good balance between performance and finding a
+    # good approximation for problems sizes up to 20,000 nodes.
+    fCool = 0.9 + 0.1*ncities/20000.0  
     if fCool > 1: fCool = 0.99   # make sure temp decreases
     maxSteps = 200 * ncities     # Number of steps at constant temperature
     maxAccepted = 20 * ncities   # Number of accepted steps at constant temperature
-    Preverse = 0.5      # How often to choose 2-opt/3-opt 
+    P_2opt_3_opt = 0.5           # How often to choose 2-opt/3-opt 
 
     # Build numpy array of coordinates (indices correspond to city labels) 
     R = np.zeros((ncities, 2))   # ncities rows, 1st col is x, 2nd col is y
@@ -142,7 +193,7 @@ def anneal(tour, cities):
             n[2] = (n[0]-1) % ncities  # index before n0 
             n[3] = (n[1]+1) % ncities  # index after n2 
             
-            if Preverse > random.random(): 
+            if P_2opt_3_opt > random.random(): 
                 # Swap 2 edges (reverse path between tour[n[0]]-tour[n[1]])
                 # compute difference relative to cost of current tour
                 diff = (Distance(R[tour[n[2]]], R[tour[n[1]]]) + 
@@ -176,15 +227,34 @@ def anneal(tour, cities):
             if accepted > maxAccepted: break
             
         print "T=%10.5f , distance= %d, accepted steps= %d" % (T, dist, accepted)
-        T *= fCool             # The system is cooled down
-        if accepted == 0: break  # If the path does not want to change any more, stop
+        T *= fCool               # The system is cooled down
+
+        # If solution can't be further improved, stop
+        if accepted == 0: break  
 
     print ("Params:  Tstart=%6.4f, fCool=%6.4f, maxSteps=%d, maxAccepted=%d" % 
           (Tstart, fCool, maxSteps, maxAccepted))
 
-    return tour
+    return tour, dist
+
+
+def write_results(filename, length, cities):
+    """
+    Writes TSP solution results in the proper format.
+
+    :param filename: (str) results filename to use
+    :param length: (int) length of TSP tour solution
+    :param cities: list of city labels in TSP solution
+    :return: writes results to disk
+    """
+    with open(filename, 'w') as f:
+        f.write('%d\n' % (length))
+        for city in cities:
+            f.write('%d\n' % (city))
+
 
 def plot_tour(T, cities, case):
+    """ Saves a plot of a given tour of cities with name 'case' to a .png file."""
     import matplotlib.pyplot as plt
     T.append(T[0])
     x_arr, y_arr = [], []
@@ -193,7 +263,7 @@ def plot_tour(T, cities, case):
         x_arr.append(x)
         y_arr.append(y)
     plt.plot(x_arr, y_arr, '.-')
-    plt.savefig('%s.png' % (case))
+    plt.savefig('plots/%s.png' % (case))
     plt.close('all')
     #plt.show()
     
@@ -214,9 +284,12 @@ if __name__=='__main__':
     start_tour = two_approx(cities)  
 
     # now optimize the tour using simulated annealing
-    final_tour = anneal(start_tour, cities)
+    final_tour, cost = anneal(start_tour, cities)
+
+    # write solution to output
+    write_results("%s.out" % case_name, cost, final_tour)
 
     # plot
-    if sys.argv[2] and sys.argv[2] == '-p':
+    if len(sys.argv) > 2 and sys.argv[2] == '-p':
         plot_tour(start_tour, cities, "start_%s" % (case_name))
         plot_tour(final_tour, cities, "final_%s" % (case_name))
